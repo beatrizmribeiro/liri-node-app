3b53e2af73b64060ac0eb15fc95c72328b50829d
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2017-10-12 19:15:47 -0700

    update keys.js

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'IgTBmA1pznZDXqik2QvbyTIbg',
+  consumer_secret: 'cwNS1lZBj5HSTH5qCrei9uNUKPWzScADAfz5va9BamJorWPODQ',
+  access_token_key: '126212596-1i10bLCx2QXk0eKBIZWegLqGad0MbaeklCgFPZLg',
+  access_token_secret: 'wQqU54GcuRIqBfXcEu64zYzQ81F2BF91ROd9WFKn4hlXQ',
 }
 
 module.exports = twitterKeys;
